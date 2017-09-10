f728ea4101d1c3d0f2c01ac025ea12b3ec955ad3
Morteza1615 <mori13796@gmail.com>
2017-09-11 00:56:31 +0430

    morteza1615

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = ""
+bot_token = "373461992:AAHQKxI14m8pvlJbmWA-Z2qFzdadm23dGuQ"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 212400472
+sudo_id = 191128096
